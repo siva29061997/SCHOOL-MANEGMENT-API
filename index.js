55f48da0fcb56dd23c4f508d4658805b029e4ef6
siva29061997 <siva29061997@gmail.com>
2022-10-14 14:30:04 +0530

    new

--- index.js
+++ index.js
@@ -50,6 +50,46 @@ app.get("/teachers", async function (req, res) {
     }
 });
 
+app.get("/users", async function (req, res) {
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB);
+
+        let reUser = await db.collection("users").find().toArray();
+
+        await connection.close();
+
+        res.json(reUser);
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        });
+        console.log(error)
+    }
+});
+
+app.get("/books", async function (req, res) {
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB);
+
+        let reUser = await db.collection("books").find().toArray();
+
+        await connection.close();
+
+        res.json(reUser);
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        });
+        console.log(error)
+    }
+});
+
 app.post("/student", async function (req, res) {
     try {
 
@@ -78,6 +118,46 @@ app.post("/teacher", async function (req, res) {
         const db = connection.db(DB)
 
         await db.collection("teachers").insertOne(req.body)
+
+        await connection.close()
+
+        res.status(200).json({ messege: "Done" })
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+        console.log(error)
+    }
+});
+
+app.post("/user", async function (req, res) {
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        await db.collection("users").insertOne(req.body)
+
+        await connection.close()
+
+        res.status(200).json({ messege: "Done" })
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+        console.log(error)
+    }
+});
+
+app.post("/book", async function (req, res) {
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        await db.collection("books").insertOne(req.body)
 
         await connection.close()
 
@@ -128,6 +208,44 @@ app.get("/teacher/:id", async function (req, res) {
     }
 });
 
+app.get("/user/:id", async function (req, res) {
+    try {
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let student = await db.collection("users").findOne({ _id: mongodb.ObjectId(req.params.id) });
+
+        await connection.close()
+
+        res.status(200).json(student)
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+        console.log(error)
+    }
+});
+
+app.get("/book/:id", async function (req, res) {
+    try {
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let student = await db.collection("books").findOne({ _id: mongodb.ObjectId(req.params.id) });
+
+        await connection.close()
+
+        res.status(200).json(student)
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+        console.log(error)
+    }
+});
+
 app.put("/student/:id", async function (req, res) {
 
     try {
@@ -168,7 +286,47 @@ app.put("/teacher/:id", async function (req, res) {
     }
 });
 
-app.delete("/student/:id",async function(req,res){
+app.put("/user/:id", async function (req, res) {
+
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let student = await db.collection("users").findOneAndUpdate({ _id: mongodb.ObjectId(req.params.id) }, { $set: req.body })
+
+        await connection.close()
+
+        res.status(200).json(student)
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+    }
+});
+
+app.put("/book/:id", async function (req, res) {
+
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let student = await db.collection("books").findOneAndUpdate({ _id: mongodb.ObjectId(req.params.id) }, { $set: req.body })
+
+        await connection.close()
+
+        res.status(200).json(student)
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+    }
+});
+
+app.delete("/student/:id", async function (req, res) {
     try {
 
         const connection = await mongoClient.connect(URL)
@@ -187,7 +345,7 @@ app.delete("/student/:id",async function(req,res){
     }
 });
 
-app.delete("/teacher/:id",async function(req,res){
+app.delete("/teacher/:id", async function (req, res) {
     try {
 
         const connection = await mongoClient.connect(URL)
@@ -195,6 +353,44 @@ app.delete("/teacher/:id",async function(req,res){
         const db = connection.db(DB)
 
         let teacher = await db.collection("teachers").findOneAndDelete({ _id: mongodb.ObjectId(req.params.id) });
+
+        await connection.close()
+
+        res.status(200).json(teacher)
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+    }
+});
+
+app.delete("/user/:id", async function (req, res) {
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let teacher = await db.collection("users").findOneAndDelete({ _id: mongodb.ObjectId(req.params.id) });
+
+        await connection.close()
+
+        res.status(200).json(teacher)
+    } catch (error) {
+        res.status(500).json({
+            messege: "Somthig went wrong"
+        })
+    }
+});
+
+app.delete("/book/:id", async function (req, res) {
+    try {
+
+        const connection = await mongoClient.connect(URL)
+
+        const db = connection.db(DB)
+
+        let teacher = await db.collection("books").findOneAndDelete({ _id: mongodb.ObjectId(req.params.id) });
 
         await connection.close()
 
